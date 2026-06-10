53b54f8b2b2f3787d1cbac5d4320ec04e5db102a
GBSss-bit <gildomarbarrosdasilva@gmail.com>
2026-06-10 18:04:11 -0300

    Configurações do PWA para Android/iOS e empacotamento Electron para Windows

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Beluna App',
+        short_name: 'Beluna',
+        theme_color: '#121214',
+        background_color: '#121214',
+        display: 'standalone',
+        scope: '/',
+        start_url: '/',
+        icons: [
+          {
+            src: 'https://cdn-icons-png.flaticon.com/512/3260/3260838.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
 })
